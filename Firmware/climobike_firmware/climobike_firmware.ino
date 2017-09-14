1455b09db893e8e7c34f5415d5dc0ad4b95fb6e2
Saulo Jacques <saulojacques@protonmail.com>
2017-09-13 22:42:34 -0300

    new pictures added

--- Firmware/climobike_firmware/climobike_firmware.ino
+++ Firmware/climobike_firmware/climobike_firmware.ino
@@ -1,12 +1,10 @@
 /* This is the Climobike project firmware.
 
-   It requires the use of SoftwareSerial, TinySerial and ?? library and assumes that you have
-   a 4800-baud serial GPS device hooked up on pins 4(rx sensor) and 3(tx sensor).
-   You can find out more info about this project and how to make your own monitoring system at
+ (insert more project info here)
    https://github.com/smjacques/climobike
 */
 
-//Add Library
+//adicionando bibliotecas
 #include <SoftwareSerial.h>
 #include <Adafruit_Sensor.h>
 #include <virtuabotixRTC.h>
@@ -18,23 +16,20 @@
 
 // Defining Variables
 
-//Humidity and Temperature - DHT22
-//#define dhtPin A1 // pino que estamos conectado
-//#define DHTTYPE DHT11 // DHT 11
-
+//Umidade e Temperatura - DHT22
 #define DHTTYPE DHT22
-#define dhtPin 9
+#define dhtPin 5
 DHT dht(dhtPin, DHTTYPE);
 
 float Umidade = 0;           //umidade relativa (%)
 float TempAr = 0;            //temperatura do ar (degrees F)
-float IndexCalor = 0; //indice de calor (degrees F)
+float IndexCalor = 0;       //indice de calor (degrees F)
 
-/*Dust Sensor (Shinyei PPD42NS)
+/* Sensor de poeira(Shinyei PPD42NS/DSM501A)
  pin 1 -> Arduino GND
  pin 3 -> Arduino 5V
- pin 2 -> Pin ~7 (P1 signal out (large particulates, 3-10 µm)
- pin 4 -> Pin ~8 (P2 signal out (small particulates, 1-2 µm)
+ pin 2 -> Pin ~7 (P1 signal out (particulado grande, 3-10 µm)
+ pin 4 -> Pin ~8 (P2 signal out (particulado pequeno, 1-2 µm)
  pin 5 -> unused!
 */
 int valP1 = 7;
@@ -55,18 +50,15 @@ unsigned long sampletime_ms = 2600; //intervalo entre medida
 unsigned long lowpulseoccupancyP1 = 0;
 unsigned long lowpulseoccupancyP2 = 0;
 
-#define ECHO_TO_SERIAL 1 ////Envia registro de dados para serial se 1, nada se 0
-
+//#define ECHO_TO_SERIAL  ////comente esta linha para desligar o ECHO
 
 // Carbon Monoxide (MQ-7)
-//#define voltage_regulator_digital_out_pin 9 (not used)
-int CO_MQ7 = A2;
-
-
-//Defining RTC
-virtuabotixRTC RTC_data(2, 5, 6); //pin order(clock, data, rst)
+//digital output (not used)
+int CO_MQ7 = A0;
 
 //GPS Module
+//   Conectar o TX do GPS (transmit) na Digital 3 do Arduino
+//   Conectar o RX do GPS (receive) na Digital 4 do Arduino
 TinyGPS gps;
 SoftwareSerial gpsSerial(3,4);
 
@@ -95,7 +87,6 @@ void setup()
   starttime = millis();
   pinMode(valP1,INPUT);
   pinMode(valP1,INPUT);
-  RTC_data.setDS1302Time(00, 00, 22, 4, 19, 8, 2017);
   
 //teste se o SD esta presente e inicia
   if (!SD.begin(chipSelect)) {
@@ -110,7 +101,7 @@ void setup()
     filename[6] = i/10 + '0';
     filename[7] = i%10 + '0';
     if (! SD.exists(filename)) {
-      // apenas abre um novo arquivo se ele nao existe
+      // abre arquivo se inexistente
       logfile = SD.open(filename, FILE_WRITE); 
       break;  // abandonar o loop!
     }
@@ -122,21 +113,6 @@ void setup()
   
   Serial.print("Logging to: ");
 Serial.println(filename);
-  
-  //Serial Monitor Data
-  Serial.print("Testing TinyGPS library v. "); Serial.println(TinyGPS::library_version());
-  Serial.println();
-  Serial.println("Sats HDOP Latitude  Longitude  Fix  Date       Time     Date Alt    Course Speed Card  Chars Sentences Checksum  Gas Level  Large Dust(3-10 µm) Small Dust(1-2 µm)  ");
-  Serial.println("          (deg)     (deg)      Age                      Age  (m)    --- from GPS ----  RX    RX        Fail");
-  Serial.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-  
-  //SD card (log) data
-  logfile.print("Testing TinyGPS library v. "); Serial.println(TinyGPS::library_version());
-  logfile.println();
-  logfile.println("Sats HDOP Latitude  Longitude  Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum  Gas Level  Large Dust(3-10 µm) Small Dust(1-2 µm)  ");
-  logfile.println("          (deg)     (deg)      Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail");
-  logfile.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-  
 }
 
 /*
@@ -218,40 +194,55 @@ static void print_str(const char *str, int len)
 
 void loop()
 {
-/*========================================================================================/
-                                            RTC
-/=======================================================================================*/
-RTC_data.updateTime();
 
-/*=======================================================================================/
-                                             GPS MODULE
-/=======================================================================================*/
+//GPS MODULE
   float flat, flon;
   unsigned long age, date, time, chars = 0;
   unsigned short sentences = 0, failed = 0;
   static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
  
-/*==================================================================================/
-                                       DHT22
-/===================================================================================*/
+//DHT22
 Umidade = dht.readHumidity();
   delay(20);  
-TempAr = dht.readTemperature(true);
+TempAr = dht.readTemperature(); //use true se farenheit
   delay(20);  
 IndexCalor = dht.computeHeatIndex(TempAr,Umidade); //indice  calculado a partir da umidade e temp do ar.
 
-/*
- ==================================================================================/
-                                       MQ-7
-/===================================================================================*/
-//reads the analaog value from the CO sensor's AOUT pin
+//MQ-7
+//apenas leitura analogica do sensor de CO (pino AOUT do sensor)
 int CO_value = analogRead(CO_MQ7);
 
-/*==================================================================================/
-                                       Data Output
-/===================================================================================*/
+//Sensor de Particulado
+float ratio = 0;
+  float concentration = 0;
+  float ratio2 = 0;
+  float concentration2 = 0;
+  float temp1 = 0;
+  float temp2 = 0;
+  voMeasured1 = analogRead(valP1);
+  voMeasured2 = analogRead(valP2);
+  
+  
+//Sensor1 (particulado grande, 3-10 µm)
+  lowpulseoccupancyP1 = lowpulseoccupancyP1 + durationP1;
+  ratio = lowpulseoccupancyP1/(sampletime_ms*10.0);     // int percentage 0 to 100
+  concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // spec sheet curve
+  temp1 = voMeasured1 * (5.0 / 1024.0);
 
- 
+//Sensor2 (particulado pequeno, 1-2 µm)
+    lowpulseoccupancyP2 = lowpulseoccupancyP2 + durationP2;
+    ratio2 = lowpulseoccupancyP2/(sampletime_ms*10.0);
+    concentration2 = 1.1*pow(ratio2,3)-3.8*pow(ratio2,2)+520*ratio2+0.62;
+    temp2 = voMeasured2 * (5.0 / 1024.0);
+
+/* Eq. linear obtida em http://www.howmuchsnow.com/arduino/airquality/
+(Chris Nafis 2012)*/
+
+  dustDensity2 = (0.17 * temp2 - 0.1)* (1000/60);
+  dustDensity1 = (0.17 * temp1 - 0.1)* (1000/60);
+  
+
+//Data Output
   print_int(gps.satellites(), TinyGPS::GPS_INVALID_SATELLITES, 5);
   print_int(gps.hdop(), TinyGPS::GPS_INVALID_HDOP, 5);
   gps.f_get_position(&flat, &flon, &age);
@@ -273,82 +264,75 @@ int CO_value = analogRead(CO_MQ7);
   print_int(failed, 0xFFFFFFFF, 9);
   Serial.println();
   
-  smartdelay(1000);
-
-  
-  // log time
-  logfile.print(RTC_data.year);
-  logfile.print("/");
-  logfile.print(RTC_data.month);
-  logfile.print("/");
-  logfile.print(RTC_data.dayofmonth);
-  logfile.print(" ");
-  logfile.print(RTC_data.hours);
-  logfile.print(":");
-  logfile.print(RTC_data.minutes);
-  logfile.print(":");
-  logfile.print(RTC_data.seconds);
-  logfile.print(",");
- #if ECHO_TO_SERIAL
-  Serial.print(",");
-  Serial.print(RTC_data.year);
-  Serial.print("/");
-  Serial.print(RTC_data.month);
-  Serial.print("/");
-  Serial.print(RTC_data.dayofmonth);
-  Serial.print(" ");
-  Serial.print(RTC_data.hours);
-  Serial.print(":");
-  Serial.print(RTC_data.minutes);
-  Serial.print(":");
-  Serial.print(RTC_data.seconds);
-  Serial.print(",");
-#endif //ECHO_TO_SERIAL
-
+ 
   //Log variables
   gps.f_get_position(&flat, &flon, &age);
   logfile.print(gps.satellites(), 5);
   logfile.print(",");
   logfile.print(gps.hdop(), 5);
   logfile.print(",");  
-  logfile.print(",");
+  logfile.print("GPS Latitude");
   logfile.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-  logfile.print(",");
+  logfile.print("GPS Longitude");
   logfile.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-  logfile.print(",");
-  logfile.print(IndexCalor);
-  logfile.print(",");
+    //Inserir data/ hora e velocidade (km/h) 
+  logfile.print("Air Temperature");
   logfile.print(TempAr);
-  logfile.print(",");
+  logfile.print("Air humidity");
   logfile.print(Umidade);
-  logfile.print(",");
+  logfile.print("Heat Index");
   logfile.print(IndexCalor);
-  logfile.print(",");
-  logfile.println(CO_value);//prints the CO value
+  logfile.print("CO Level");
+  logfile.print(CO_value);//prints dos valores de CO
+  logfile.print("Large Dust Density (µg/m3)");
+  logfile.print(dustDensity1);
+  logfile.print("Large Dust Ratio (3-10µm)");
+  logfile.print(ratio);
+  logfile.print("Large Dust Concentration (3-10 µm)");
+  logfile.print(concentration);
+  logfile.print("Fine Dust Density (1-2 µm)");
+  logfile.print(dustDensity2); // unit: ug/m3
+  logfile.print("Fine Dust Ratio (µg/m3)");
+  logfile.print(ratio2);
+  logfile.print("Fine Dust Concentration (1-2 µm)");
+  logfile.println(concentration2);
   
-#if ECHO_TO_SERIAL
+  
+#ifdef ECHO_TO_SERIAL
   Serial.print(gps.satellites(), 5);
   Serial.print(",");
   Serial.print(gps.hdop(), 5);
   Serial.print(",");  
-  Serial.print(",");
+  Serial.print("GPS Latitude");
   Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-  Serial.print(",");
+  Serial.print("GPS Longitude");
   Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-  Serial.print(",");
-  Serial.print(IndexCalor);
-  Serial.print(",");
-  Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-  Serial.print(",");
-  Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);  
+  //Inserir data/ hora e velocidade (km/h) 
+  Serial.print("Air Temperature");
   Serial.print(TempAr);
-  Serial.print(",");
+  Serial.print("Air Humidity");
   Serial.print(Umidade);
-  Serial.print(",");
+  Serial.print("Heat Index");
   Serial.print(IndexCalor);
-  Serial.print(",");
-  Serial.println(CO_value);//prints the CO value
+  Serial.print("CO level");
+  Serial.print(CO_value);
+  Serial.print("Large Dust Density (µg/m3)");
+  Serial.print(dustDensity1);
+  Serial.print("Large Dust Ratio (3-10µm)");
+  Serial.print(ratio);
+  Serial.print("Large Dust Concentration (3-10 µm)");
+  Serial.print(concentration);
+  Serial.print("Fine Dust Density (1-2 µm)");
+  Serial.print(dustDensity2); // unit: ug/m3
+  Serial.print("Fine Dust Ratio (µg/m3)");
+  Serial.print(ratio2);
+  Serial.print("Fine Dust Concentration (1-2 µm)");
+  Serial.println(concentration2);
   
 #endif
+  Serial.println("");
 
+
+// delay
+  smartdelay(1000);
 }
---
+++ Firmware/sensors - debug/microSD_DHT/microSD_DHT.ino
@@ -0,0 +1,64 @@
+#include <SD.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+ 
+const int chipSelect = 10; //MicroSD pin
+//Constants
+// DHT 22  (AM2302) - what pin we're connected to
+#define DHTTYPE DHT22
+#define dhtPin 5
+DHT dht(dhtPin, DHTTYPE);
+//Variables
+float Umidade = 0;           //umidade relativa (%)
+float TempAr = 0;            //temperatura do ar (degrees F)
+float IndexCalor = 0; //indice de calor (degrees F)
+ 
+void setup()
+{
+ 
+Serial.begin(9600);
+while (!Serial) {
+; // wait for serial port to connect.
+}
+Serial.print("Initializing SD card…");
+pinMode(10, OUTPUT);
+
+//init SD card
+if (!SD.begin(chipSelect))
+{
+Serial.println("Card failed, or not present");
+return;
+}
+Serial.println("card initialized.");
+dht.begin();
+}
+ 
+void loop()
+{
+  
+Umidade = dht.readHumidity();
+  delay(20);  
+TempAr = dht.readTemperature(true);
+  delay(20);  
+IndexCalor = dht.computeHeatIndex(TempAr,Umidade);
+
+// open the file.
+File dataFile = SD.open("temp.txt", FILE_WRITE);
+ 
+// if the file is available, write to it:
+if (dataFile)
+{
+dataFile.println("Umidade do Ar = ");
+dataFile.println(Umidade);
+dataFile.println("%  ");
+dataFile.println("Temperatura = ");
+dataFile.println(TempAr);
+dataFile.println("C  ");
+dataFile.close();
+}
+// if the file isn’t open
+else
+{
+Serial.println("error opening temp.txt");
+}
+}
